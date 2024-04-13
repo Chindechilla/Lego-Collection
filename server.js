f512b1fac114cb4cbcb01c512035efda29be883e
Chindechilla <vutrinh0423@gmail.com>
2024-04-13 00:00:59 -0400

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /********************************************************************************
-* WEB322 – Assignment 05
+* WEB322 – Assignment 06
 * 
 * I declare that this assignment is my own work in accordance with Seneca's
 * Academic Integrity Policy:
@@ -13,15 +13,21 @@
 ********************************************************************************/
 const legoData = require("./modules/legoSets");
 const path = require("path");
+const mongoose = require('mongoose');
+const authData = require('./modules/auth-service');
 
+const bcrypt = require('bcryptjs');
+const clientSessions = require('client-sessions');
 const express = require('express');
 const app = express();
-
+const uri = "mongodb+srv://vutrinh0423:trinh250600@chindechilla.fbyeaf5.mongodb.net/?retryWrites=true&w=majority&appName=Chindechilla"
 const HTTP_PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.error('MongoDB connection error:', err));
 app.get('/', (req, res) => {
   res.render("home")
 });
@@ -30,8 +36,63 @@ app.get('/about', (req, res) => {
   res.render("about");
 });
 
+const ensureLogin = (req, res, next) => {
+  if (!req.session || !req.session.userName) {
+      res.redirect('/login');
+  } else {
+      next();
+  }
+};
+// GET route to render the login view
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// GET route to render the register view
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+// POST route to handle user registration
+app.post('/register', async (req, res) => {
+  try {
+    const userData = req.body;
+    await authData.registerUser(userData);
+    res.render('register', { successMessage: 'User created' });
+  } catch (err) {
+    res.render('register', { errorMessage: err, userName: req.body.userName });
+  }
+});
+
+// POST route to handle user login
+app.post('/login', async (req, res) => {
+  try {
+    req.body.userAgent = req.get('User-Agent');
+    const user = await authData.checkUser(req.body);
+    req.session.user = {
+      userName: user.userName,
+      email: user.email,
+      loginHistory: user.loginHistory
+    };
+    res.redirect('/lego/sets');
+  } catch (err) {
+    res.render('login', { errorMessage: err, userName: req.body.userName });
+  }
+});
+
+// GET route to logout
+app.get('/logout', (req, res) => {
+  req.session.reset();
+  res.redirect('/');
+});
+
+// GET route to render user history view
+app.get('/userHistory', ensureLogin, (req, res) => {
+  res.render('userHistory');
+});
+
 // Route to render the addSet view with themes
-app.get("/lego/addSet", async (req, res) => {
+app.get("/lego/addSet", ensureLogin, async (req, res) => {
   try {
     const themes = await legoData.getAllThemes();
     res.render("addSet", { themes: themes });
@@ -40,7 +101,7 @@ app.get("/lego/addSet", async (req, res) => {
   }
 });
 // Route to handle form submission and add a new set
-app.post("/lego/addSet", async (req, res) => {
+app.post("/lego/addSet", ensureLogin, async (req, res) => {
   try {
     const setData = req.body;
     await legoData.addSet(setData);
@@ -50,7 +111,7 @@ app.post("/lego/addSet", async (req, res) => {
   }
 });
 // Define routes for editing a set
-app.get("/lego/editSet/:num", async (req, res) => {
+app.get("/lego/editSet/:num", ensureLogin, async (req, res) => {
   try {
     const setNum = req.params.num;
     const set = await legoData.getSetByNum(setNum);
@@ -61,7 +122,7 @@ app.get("/lego/editSet/:num", async (req, res) => {
   }
 });
 
-app.post("/lego/editSet", async (req, res) => {
+app.post("/lego/editSet", ensureLogin, async (req, res) => {
   try {
     const setNum = req.body.set_num;
     const setData = {
@@ -77,7 +138,7 @@ app.post("/lego/editSet", async (req, res) => {
     res.status(500).render('500', { message: `I'm sorry, but we have encountered the following error: ${err}` });
   }
 });
-app.get("/lego/deleteSet/:num", async (req, res) => {
+app.get("/lego/deleteSet/:num", ensureLogin, async (req, res) => {
   try {
       const setNum = req.params.num;
       console.log("Set number to delete:", setNum);
@@ -120,7 +181,24 @@ app.get("/lego/sets/:num", async (req,res)=>{
 app.use((req, res, next) => {
   res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
 });
+Promise.all([authData.initialize(), legoData.initialize()])
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log(`Server is running on port ${HTTP_PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("Unable to start the server:", err);
+  });
 
-legoData.initialize().then(()=>{
-  app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
+app.use(clientSessions({
+  cookieName: 'session',
+  secret: 'your_secret_string',
+  duration: 24 * 60 * 60 * 1000, // 24 hours
+  activeDuration: 1000 * 60 * 5 // 5 minutes
+}));
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
 });
+
