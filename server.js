4310fbd107a853cc639b62ea9fd75b80d7fa69af
Chindechilla <vutrinh0423@gmail.com>
2024-05-05 00:16:49 -0400

    updated

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
 );
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname, 'public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
