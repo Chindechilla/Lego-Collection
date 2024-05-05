79bb808b271887f1b044cff73ee545830bf302a1
Chindechilla <vutrinh0423@gmail.com>
2024-05-05 00:09:21 -0400

    error

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("home");
