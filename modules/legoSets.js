a5123a303db9bde451a44823b4ad5285f8742c4b
Chindechilla <vutrinh0423@gmail.com>
2024-05-04 23:41:04 -0400

    .

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+const pg = require('pg'); 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
